20700e96b1e4b5234710b9753cae2e06bc893b0a
Rachel Laycock <rachel.laycock@gmail.com>
2012-09-28 18:59:27 +0200

    Rachel and Charles: Refactored file store to use dependency injection and cleaned up magic numbers

--- file_store.py
+++ file_store.py
@@ -4,11 +4,12 @@ import io
 __author__ = 'rlaycock'
 
 class FileStore(object):
-    def __init__(self):
+    def __init__(self, hasher=hashlib.md5):
         self.file_set = set()
+        self.hasher = hasher
 
     def Add(self, file_name):
         with io.open(file_name) as f:
-            hash = hashlib.md5(f.read())
+            hash = self.hasher(f.read())
             if hash not in self.file_set:
                 self.file_set.add(hash)
--- test_file_store.py
+++ test_file_store.py
@@ -2,33 +2,39 @@ import io
 import file_store
 from mock import MagicMock, patch
 
+DIFFERENT_HASH_RETURN_VALUE = 123456
+
+HASH_RETURN_VALUE = 12345
+
+HASH_RETURN_VALUE = 12345
+
 __author__ = 'rlaycock'
 
 import unittest
 
-@patch('hashlib.md5', MagicMock(return_value=12345))
 @patch('io.open', MagicMock())
 class FileStoreTest(unittest.TestCase):
     def setUp(self):
-        self.store = file_store.FileStore()
+        self.mockHash = MagicMock(return_value=HASH_RETURN_VALUE)
+        self.store = file_store.FileStore(self.mockHash)
 
     def test_can_add_file_to_file_store(self):
         self.store.Add("myFile")
 
-        self.assertEqual(self.store.file_set, { 12345 })
+        self.assertEqual(self.store.file_set, {HASH_RETURN_VALUE})
 
     def test_adding_different_file_adds_another_entry_to_the_dictionary(self):
         self.store.Add("myFile")
-        with patch('hashlib.md5', MagicMock(return_value=123456)):
-            self.store.Add("myOtherFile")
+        self.mockHash.return_value= DIFFERENT_HASH_RETURN_VALUE
+        self.store.Add("myOtherFile")
 
-        self.assertEqual(self.store.file_set, { 12345, 123456 })
+        self.assertEqual(self.store.file_set, { HASH_RETURN_VALUE, DIFFERENT_HASH_RETURN_VALUE})
 
     def test_adding_two_files_with_different_name_same_content_only_adds_first_file(self):
         self.store.Add("myFile")
         self.store.Add("myFileWithSameContent")
 
-        self.assertEqual(self.store.file_set, { 12345 })
+        self.assertEqual(self.store.file_set, { HASH_RETURN_VALUE })
 
 if __name__ == '__main__':
     unittest.main()
